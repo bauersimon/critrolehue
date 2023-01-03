04bbdb03e11a53d1cd9c66043b48b6fe22e8b157
Simon Bauer <sbauer@cs.uni-salzburg.at>
2023-01-03 22:20:19 +0100

    Adapt mask based on face detection
    I.e. remove head areas from a mask to avoid having
    hair/skin tones in the final color estimates.

---
+++ extractor/face.py
@@ -0,0 +1,50 @@
+import cv2
+import numpy as np
+from typing import List, Tuple
+from paths import ROOT
+from os import path
+from data import load_frame, show_frame, save_frame
+
+
+def _extract_faces(frame: np.array) -> List[Tuple[int, int, int, int]]:
+    """Extract a list of `(x,y,w,h)`."""
+    # pylint: disable=no-member (Pylint cannot find any members within cv2.)
+
+    frame = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
+    classifier = cv2.CascadeClassifier(path.join(ROOT, 'face_model.xml'))
+    faces = classifier.detectMultiScale(
+        image=frame,
+        scaleFactor=1.1,
+        minNeighbors=5,
+    )
+
+    return faces
+
+
+def _head_mask(faces: List[Tuple[int, int, int, int]], height: int, width: int, radius_factor: float) -> np.array:
+    # pylint: disable=no-member (Pylint cannot find any members within cv2.)
+    mask = np.zeros((height, width, 3))
+    for (x, y, w, h) in faces:
+        center = (int(x + 0.5*w), int(y + 0.4*h))
+        radius = np.max([w, h])
+        mask = cv2.circle(
+            mask,
+            center,
+            int(radius * radius_factor),
+            (255, 255, 255),
+            -1,
+        )
+
+    return mask
+
+
+def remove_heads_from_mask(frame: np.array, mask: np.array, head_radius=0.8) -> np.array:
+    faces = _extract_faces(frame)
+    height = frame.shape[0]
+    width = frame.shape[1]
+    head_mask = _head_mask(faces, height, width, head_radius)
+
+    mask = mask - head_mask
+    mask = np.where(mask >= 0, mask, np.zeros_like(mask))
+
+    return mask
---
+++ extractor/test_face.py
@@ -0,0 +1,25 @@
+import unittest
+from data import load_frame
+from face import _extract_faces, _head_mask, remove_heads_from_mask
+from numpy import testing as nptest
+from os import path
+from paths import TESTING
+
+
+class TestHeadMask(unittest.TestCase):
+
+    def test_compute_heads(self):
+        f = load_frame(path.join(TESTING, "night_0_5.png"))
+        faces = _extract_faces(f)
+        actual = _head_mask(faces, f.shape[0], f.shape[1], 0.8)
+        expected = load_frame(path.join(TESTING, "night_0_5_faces.png"))
+
+        nptest.assert_equal(actual, expected)
+
+    def test_remove_heads(self):
+        f = load_frame(path.join(TESTING, "night_0_5.png"))
+        m = load_frame(path.join(TESTING, "mask_windows.png"))
+        actual = remove_heads_from_mask(f, m, 1.5)
+        expected = load_frame(path.join(TESTING, "night_0_5_mask.png"))
+
+        nptest.assert_equal(actual, expected)
