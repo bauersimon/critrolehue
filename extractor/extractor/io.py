cf4ca969e0fc39a7b7bdd3a30462ddedfbae7677
Simon Bauer <sbauer@cs.uni-salzburg.at>
2023-12-28 12:32:08 +0100

    well-defined structure for how frame references are to be stored

--- extractor/extractor/io.py
+++ extractor/extractor/io.py
@@ -1,7 +1,13 @@
+import logging
+import os
+from os import path
+
 import cv2
 import numpy as np
 import numpy.typing as npt
 from matplotlib import pyplot as plt
+
+logger = logging.getLogger(__name__)
 
 
 def get_frame(file: str, second: float) -> npt.NDArray:
@@ -35,6 +41,7 @@ def show_frame(frame: npt.NDArray):
 
 
 def save_frame(file: str, frame: npt.NDArray):
+    frame = frame.astype(np.float32)
     frame = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
     cv2.imwrite(file, frame)
 
@@ -43,3 +50,35 @@ def load_frame(file: str) -> npt.NDArray:
     frame = cv2.imread(file)
     frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
     return np.array(frame)
+
+
+def find_frames(frame_dir: str, quality: str) -> tuple[npt.NDArray, npt.NDArray, list[npt.NDArray], list[npt.NDArray]]:
+    """
+    'frame_(mask)_(fullhd|hd|sd).png' define valid frames and '(hue|temp)_<name>_(fullhd|hd|sd).png' define hue/temperature areas (in alphabetic order)
+
+    Returns a tuple of the form: `(frame_mask, frame, hues, temps)`.
+    """
+    frame_raw: str = ""
+    frame_mask: str = ""
+    hues: list[str] = []
+    temps: list[str] = []
+    for frame in sorted(os.listdir(frame_dir)):
+        if frame == f"frame_mask_{quality}.png":
+            logging.debug(f"Registered frame mask: {frame}")
+            frame_mask = path.join(frame_dir, frame)
+        elif frame == f"frame_{quality}.png":
+            logging.debug(f"Registered frame content: {frame}")
+            frame_raw = path.join(frame_dir, frame)
+        elif frame.startswith("hue_") and frame.endswith(f"_{quality}.png"):
+            logging.debug(f"Registered hue area: {frame}")
+            hues.append(path.join(frame_dir, frame))
+        elif frame.startswith("temp_") and frame.endswith(f"_{quality}.png"):
+            logging.debug(f"Registered temperature area: {frame}")
+            temps.append(path.join(frame_dir, frame))
+
+    return (
+        load_frame(frame_mask),
+        load_frame(frame_raw),
+        list(map(load_frame, hues)),
+        list(map(load_frame, temps)),
+    )
--- extractor/tests/test_io.py
+++ extractor/tests/test_io.py
@@ -1,6 +1,8 @@
+import tempfile
 import unittest
 from os import path
 
+import numpy as np
 from numpy import testing as nptest
 
 from extractor import io
@@ -35,6 +37,30 @@ class TestGetFrame(unittest.TestCase):
             2.0,
             path.join(TESTING, "night_2_0.png"),
         )
+
+
+class TestFindFrames(unittest.TestCase):
+    def test_find_frames(self):
+        frame_mask = np.ones((720, 1280, 3))
+        frame = np.ones((720, 1280, 3))+1
+        hue = np.ones((720, 1280, 3))+2
+        temp = np.ones((720, 1280, 3))+3
+
+        with tempfile.TemporaryDirectory() as dir:
+            io.save_frame(path.join(dir, "frame_mask_hd.png"), frame_mask)
+            io.save_frame(path.join(dir, "frame_hd.png"), frame)
+            io.save_frame(path.join(dir, "hue_foo_hd.png"), hue)
+            io.save_frame(path.join(dir, "temp_foo_hd.png"), temp)
+
+            frame_mask_actual, frame_actual, hues, temps = io.find_frames(
+                dir, "hd")
+
+            nptest.assert_equal(frame_mask, frame_mask_actual)
+            nptest.assert_equal(frame, frame_actual)
+            self.assertEqual(len(hues), 1)
+            nptest.assert_equal(hue, hues[0])
+            self.assertEqual(len(temps), 1)
+            nptest.assert_equal(temp, temps[0])
 
 
 if __name__ == "__main__":
